8f6a30655daec7eb252d342470fedfaf3de202e1
desafin <131871057+desafin@users.noreply.github.com>
2023-11-17 16:48:12 +0900

    11 17 오후 5시

--- flickable_example/main.cpp
+++ flickable_example/main.cpp
@@ -7,12 +7,8 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/flickable_example/main.qml"_qs);
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-        &app, [url](QObject *obj, const QUrl &objUrl) {
-            if (!obj && url == objUrl)
-                QCoreApplication::exit(-1);
-        }, Qt::QueuedConnection);
+    const QUrl url("qrc:/flickable_example/main.qml");
+
     engine.load(url);
 
     return app.exec();
